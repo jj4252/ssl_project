6e637717c804e20f5eab806827575cb1df8ddc3b
jj4252 <jj4252@nyu.edu>
2025-11-21 19:36:36 -0500

    Add files via upload

--- contra_moco/evaluate_testset1.py
+++ contra_moco/evaluate_testset1.py
@@ -12,6 +12,7 @@ import argparse
 import yaml
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
